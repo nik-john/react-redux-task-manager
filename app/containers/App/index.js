fc90c6f1b78806e72849c8ebdf748c468c6f3990
Nikhil John <nikhil.john.88@gmail.com>
2018-06-13 05:28:28 +0530

    Fix broken import

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,16 +8,50 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 // import TasksList from 'containers/TasksList/Loadable';
 import Tasks from 'containers/Tasks';
-import { AppWrapper, theme } from 'containers/App/Styles';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 
 // import Footer from 'components/Footer';
+
+import { media } from '../../global-styles';
+
+const AppWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  padding: 0 16px;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  width: 100%;
+  position: relative;
+  padding-top: 55px;
+  padding-bottom: 20px;
+  color: #444;
+  min-width: 300px;
+  ${media.desktop`
+    width: calc(100vh - 100px);
+  `} 
+`;
+
+const theme = {
+  blue: '#22A4F2',
+  white: '#ffffff',
+  green: '#2AC772',
+  red: '#F26749',
+  grey: '#ADC0CC',
+  lightGrey: '#6B7D87',
+  lighterGrey: '#A4BBC8',
+  navyBlue: 'rgba(26, 61, 82, 1)',
+  textDark: '#444444',
+  borderDefault: 'rgba(189, 189, 189, 0.5)',
+  boxShadowDefault: '0 0 8px 0 rgba(0,0,0,.3)',
+  boxShadowLight: '0px 2px 3px rgba(0, 0, 0, 0.05)',
+};
 
 
 export default function App() {
